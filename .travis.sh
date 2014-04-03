275301b2a6417a6cc6e8b0545b059e19c53f30c2
François Poirotte <clicky@erebot.net>
2014-04-04 00:35:08 +0200

    Another attempt at fixing hhvm builds.

--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-wget -O- http://dl.hhvm.com/conf/hhvm.gpg.key | apt-key add -
-add-apt-repository "deb http://dl.hhvm.com/ubuntu precise main"
 apt-get remove hhvm
 add-apt-repository -y ppa:mapnik/boost
+wget -O- http://dl.hhvm.com/conf/hhvm.gpg.key | apt-key add -
+echo deb http://dl.hhvm.com/ubuntu precise main | tee /etc/apt/sources.list.d/hhvm.list
 apt-get update -q
 apt-get install hhvm-nightly
