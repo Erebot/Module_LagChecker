ac757ea1d33aa9dd874e0aacaf746778c0a980ad
François Poirotte <clicky@erebot.net>
2014-04-04 00:51:02 +0200

    Real quiet.

--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-apt-get remove hhvm
+apt-get remove -qq hhvm
 add-apt-repository -y ppa:mapnik/boost
 wget -O- http://dl.hhvm.com/conf/hhvm.gpg.key | apt-key add -
 echo deb http://dl.hhvm.com/ubuntu precise main | tee /etc/apt/sources.list.d/hhvm.list
-apt-get update -q
-apt-get install --assume-no hhvm-nightly
+apt-get update -qq
+apt-get install -qq hhvm-nightly
