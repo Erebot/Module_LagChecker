d6be5b0deff2c7c32c6ef5d85f21639b76d49c65
François Poirotte <clicky@erebot.net>
2014-04-04 00:42:34 +0200

    Don't block the build with questions.

--- .travis.sh
+++ .travis.sh
@@ -5,4 +5,4 @@ add-apt-repository -y ppa:mapnik/boost
 wget -O- http://dl.hhvm.com/conf/hhvm.gpg.key | apt-key add -
 echo deb http://dl.hhvm.com/ubuntu precise main | tee /etc/apt/sources.list.d/hhvm.list
 apt-get update -q
-apt-get install hhvm-nightly
+apt-get install --assume-no hhvm-nightly
